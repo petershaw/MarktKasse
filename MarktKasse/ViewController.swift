97cbfd8300ecab4acfd6fdec6c3c8ff7e28e84a6
Kris <kris@opentabs.de>
2015-05-07 22:13:00 +0200

    Initial Commit

---
+++ MarktKasse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarktKasse
+//
+//  Created by Kris Wolff on 07/05/15.
+//  Copyright (c) 2015 aus der Technik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MarktKasseTests/MarktKasseTests.swift
@@ -0,0 +1,36 @@
+//
+//  MarktKasseTests.swift
+//  MarktKasseTests
+//
+//  Created by Kris Wolff on 07/05/15.
+//  Copyright (c) 2015 aus der Technik. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MarktKasseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
